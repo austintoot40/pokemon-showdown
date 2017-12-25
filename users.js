austintoot40/pokemon-showdown

commit c4fc337ceb00f28a9a8a746772a6c1f7924309ea

Improve trusted user handling
Trusted users should no longer get caught in namelocks, etc.

users.js
@@ -394,6 +394,7 @@ class User {
 		this.locked = false;
 		this.semilocked = false;
 		this.namelocked = false;
+		this.permalocked = false;
 		this.prevNames = Object.create(null);
 		this.inRooms = new Set();
 
@@ -731,6 +732,17 @@ class User {
 				Punishments.ban(this, Date.now() + PERMALOCK_CACHE_TIME, userid, `Permabanned as ${name}`);
 			}
 		}
+		if (Users.isTrusted(userid)) {
+			this.trusted = this.userid;
+			this.autoconfirmed = this.userid;
+		}
+		if (this.trusted) {
+			this.locked = null;
+			this.namelocked = null;
+			this.permalocked = null;
+			this.semilocked = null;
+		}
+
 		let user = users.get(userid);
 		let possibleUser = Users(userid);
 		if (possibleUser && possibleUser.namelocked) {
@@ -935,11 +947,6 @@ class User {
 			this.group = usergroups[this.userid].charAt(0);
 		} else {
 			this.group = Config.groupsranking[0];
-		}
-
-		if (Users.isTrusted(this)) {
-			this.trusted = this.userid;
-			this.autoconfirmed = this.userid;
 		}
 
 		if (Config.customavatars && Config.customavatars[this.userid]) {

commit 270d250aed1833583ceff10d21d12d155f31019d

Users: Fix marking guest numbers as autoconfirmed (#4278)
This is a bug that was introduced in https://github.com/Zarel/Pokemon-Showdown/commit/c4fc337ceb00f28a9a8a746772a6c1f7924309ea

users.js
@@ -733,8 +733,8 @@ class User {
 			}
 		}
 		if (Users.isTrusted(userid)) {
-			this.trusted = this.userid;
-			this.autoconfirmed = this.userid;
+			this.trusted = userid;
+			this.autoconfirmed = userid;
 		}
 		if (this.trusted) {
 			this.locked = null;