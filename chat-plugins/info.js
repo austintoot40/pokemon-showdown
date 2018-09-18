austintoot40/pokemon-showdown

commit 05d2817abeed2c0b8c4855e0e66c557f44f6d73a

Add basic language support (#4862)

chat-plugins/info.js
@@ -1682,11 +1682,25 @@ const commands = {
 	rule: 'rules',
 	rules: function (target, room, user) {
 		if (!target) {
+			const languageTable = {
+				portuguese: ['Por favor siga as regras:', 'pages/rules-pt', 'Regras Globais', `Regras da sala ${room.title}`],
+				spanish: ['Por favor sigue las reglas:', 'pages/rules-es', 'Reglas Globales', `Reglas de la sala ${room.title}`],
+				italian: ['Per favore, rispetta le seguenti regole:', 'pages/rules-it', 'Regole Globali', `Regole della room ${room.title}`],
+				french: ['Veuillez suivre ces règles:', 'pages/rules-fr', 'Règles Générales', `Règles de la room ${room.title}`],
+				simplifiedchinese: ['请遵守规则:', 'pages/rules-zh', '全站规则', `${room.title}房间规则`],
+				traditionalchinese: ['請遵守規則:', 'pages/rules-tw', '全站規則', `${room.title}房間規則`],
+				japanese: ['ルールを守ってください:', 'pages/rules-ja', '全部屋共通ルール', `${room.title}部屋のルール`],
+				hindi: ['कृपया इन नियमों का पालन करें:', 'pages/rules-hi', 'आप सभी के लिए नियम:', `${room.title} इस Room के नियम:`],
+				turkish: ['Lütfen kurallara uyun:', 'pages/rules-tr', 'Genel kurallar', `${room.title} odası kuralları`],
+				dutch: ['Volg de regels:', 'pages/rules-nl', 'Globale Regels ', `Regels van de ${room.title} room`],
+				german: ['Bitte befolgt die Regeln:', 'pages/rules-de', 'Globale Regeln', `Regeln des ${room.title} Raumes`],
+				english: ['Please follow the rules:', 'rules', 'Global Rules', `${room.title} room rules`],
+			};
 			if (!this.runBroadcast()) return;
 			this.sendReplyBox(
-				`Please follow the rules:<br />` +
-				(room && room.rulesLink ? Chat.html`- <a href="${room.rulesLink}">${room.title} room rules</a><br />` : ``) +
-				`- <a href="https://pokemonshowdown.com/rules">${room && room.rulesLink ? "Global rules" : "Rules"}</a>`
+				`${languageTable[room.language || 'english'][0]}<br />` +
+				(room && room.rulesLink ? Chat.html`- <a href="${room.rulesLink}">${languageTable[room.language || 'english'][3]}</a><br />` : ``) +
+				`- <a href="https://pokemonshowdown.com/${languageTable[room.language || 'english'][1]}">${languageTable[room.language || 'english'][2]}</a>`
 			);
 			return;
 		}
@@ -1845,11 +1859,21 @@ const commands = {
 			} else if (extraFormat.effectType !== 'Format') {
 				formatName = formatId = '';
 			}
+			const supportedLanguages = {
+				spanish: 'es',
+				french: 'fr',
+				italian: 'ita',
+				german: 'ger',
+				portuguese: 'por',
+			};
 			let speciesid = pokemon.speciesid;
 			// Special case for Meowstic-M
 			if (speciesid === 'meowstic') speciesid = 'meowsticm';
 			if (pokemon.tier === 'CAP') {
 				this.sendReplyBox(`<a href="https://www.smogon.com/cap/pokemon/strategies/${speciesid}">${generation.toUpperCase()} ${Chat.escapeHTML(formatName)} ${pokemon.name} analysis preview</a>, brought to you by <a href="https://www.smogon.com">Smogon University</a> <a href="http://smogon.com/cap/">CAP Project</a>`);
+			} else if (formatId === 'ou' && generation === 'sm' && room.language in supportedLanguages) {
+				// Limited support for translated analysis
+				this.sendReplyBox(`<a href="https://www.smogon.com/translations/${supportedLanguages[room.language]}/analyses/ou/${speciesid}">${generation.toUpperCase()} ${Chat.escapeHTML(formatName)} ${pokemon.name} analysis</a>, brought to you by <a href="https://www.smogon.com">Smogon University</a>`);
 			} else {
 				this.sendReplyBox(`<a href="https://www.smogon.com/dex/${generation}/pokemon/${speciesid}${(formatId ? '/' + formatId : '')}">${generation.toUpperCase()} ${Chat.escapeHTML(formatName)} ${pokemon.name} analysis</a>, brought to you by <a href="https://www.smogon.com">Smogon University</a>`);
 			}

chat-plugins/roomsettings.js
@@ -157,8 +157,20 @@ class RoomSettings {
 			return `${this.button('Mafia enabled', null, 'mafia enable')} ${this.button('off', true)}`;
 		}
 	}
+	language() {
+		const languageList = ['Portuguese', 'Spanish', 'Italian', 'French', 'Simplified Chinese', 'Traditional Chinese', 'Japanese', 'Hindi', 'Turkish', 'Dutch', 'German'];
+		if (!this.user.can('editroom', null, this.room)) return this.button(this.room.language ? this.room.language : 'English', true);
+
+		let languageOutput = [];
+		languageOutput.push(this.button(`English`, !this.room.language, 'roomlanguage english'));
+		for (let language of languageList) {
+			languageOutput.push(this.button(`${language}`, this.room.language === toId(language), `roomlanguage ${toId(language)}`));
+		}
+		return languageOutput.join(' ');
+	}
 	generateDisplay(user, room, connection) {
 		let output = Chat.html`<div class="infobox">Room Settings for ${this.room.title}<br />`;
+		output += `<strong>Language:</strong> <br />${this.language()}<br />`;
 		output += `<strong>Modchat:</strong> <br />${this.modchat()}<br />`;
 		output += `<strong>Modjoin:</strong> <br />${this.modjoin()}<br />`;
 		output += `<strong>Stretch filter:</strong> <br />${this.stretching()}<br />`;
@@ -347,6 +359,42 @@ exports.commands = {
 	modjoinhelp: [
 		`/modjoin [+|%|@|*|player|&|~|#|off] - Sets modjoin. Users lower than the specified rank can't join this room unless they have a room rank. Requires: \u2606 # & ~`,
 		`/modjoin [sync|off] - Sets modjoin. Only users who can speak in modchat can join this room. Requires: \u2606 # & ~`,
+	],
+
+	roomlanguage: function (target, room, user) {
+		const languageTable = {
+			portuguese: 'Portuguese',
+			spanish: 'Spanish',
+			italian: 'Italian',
+			french: 'French',
+			simplifiedchinese: 'Simplified Chinese',
+			traditionalchinese: 'Traditional Chinese',
+			japanese: 'Japanese',
+			hindi: 'Hindi',
+			turkish: 'Turkish',
+			dutch: 'Dutch',
+			german: 'German',
+			// Listed as "false" under room.language
+			english: 'English',
+		};
+		if (!target) return this.sendReply(`This room's primary language is ${languageTable[room.language] || 'English'}`);
+		if (!this.can('editroom', null, room)) return false;
+
+		let targetLanguage = toId(target);
+		if (!(targetLanguage in languageTable)) return this.errorReply(`"${target}" is not a supported language.`);
+
+		room.language = targetLanguage === 'english' ? false : targetLanguage;
+
+		if (room.chatRoomData) {
+			room.chatRoomData.language = room.language;
+			Rooms.global.writeChatRoomData();
+		}
+		this.modlog(`LANGUAGE`, null, languageTable[targetLanguage]);
+		this.sendReply(`The room's language has been set to ${languageTable[targetLanguage]}`);
+	},
+	roomlanguagehelp: [
+		`/roomlanguage [language] - Sets the the language for the room, which changes language of a few commands. Requires # & ~`,
+		`Supported Languages: English, Spanish, Italian, French, Simplified Chinese, Traditional Chinese, Japanese, Hindi, Turkish, Dutch, German.`,
 	],
 
 	slowchat: function (target, room, user) {

rooms.js
@@ -101,6 +101,8 @@ class BasicRoom {
 		/** @type {string?} */
 		this.modchat = null;
 		this.staffRoom = false;
+		/** @type {string | false} */
+		this.language = false;
 		/** @type {false | number} */
 		this.slowchat = false;
 		this.filterStretching = false;

commit ecc4f553870efe6c97a394b1340e77b7f3e38ef1

Rules: Fix crash when used in PMs (#4863)
Introduced in https://github.com/Zarel/Pokemon-Showdown/commit/05d2817abeed2c0b8c4855e0e66c557f44f6d73a

chat-plugins/info.js
@@ -1683,24 +1683,26 @@ const commands = {
 	rules: function (target, room, user) {
 		if (!target) {
 			const languageTable = {
-				portuguese: ['Por favor siga as regras:', 'pages/rules-pt', 'Regras Globais', `Regras da sala ${room.title}`],
-				spanish: ['Por favor sigue las reglas:', 'pages/rules-es', 'Reglas Globales', `Reglas de la sala ${room.title}`],
-				italian: ['Per favore, rispetta le seguenti regole:', 'pages/rules-it', 'Regole Globali', `Regole della room ${room.title}`],
-				french: ['Veuillez suivre ces règles:', 'pages/rules-fr', 'Règles Générales', `Règles de la room ${room.title}`],
-				simplifiedchinese: ['请遵守规则:', 'pages/rules-zh', '全站规则', `${room.title}房间规则`],
-				traditionalchinese: ['請遵守規則:', 'pages/rules-tw', '全站規則', `${room.title}房間規則`],
-				japanese: ['ルールを守ってください:', 'pages/rules-ja', '全部屋共通ルール', `${room.title}部屋のルール`],
-				hindi: ['कृपया इन नियमों का पालन करें:', 'pages/rules-hi', 'आप सभी के लिए नियम:', `${room.title} इस Room के नियम:`],
-				turkish: ['Lütfen kurallara uyun:', 'pages/rules-tr', 'Genel kurallar', `${room.title} odası kuralları`],
-				dutch: ['Volg de regels:', 'pages/rules-nl', 'Globale Regels ', `Regels van de ${room.title} room`],
-				german: ['Bitte befolgt die Regeln:', 'pages/rules-de', 'Globale Regeln', `Regeln des ${room.title} Raumes`],
-				english: ['Please follow the rules:', 'rules', 'Global Rules', `${room.title} room rules`],
+				portuguese: ['Por favor siga as regras:', 'pages/rules-pt', 'Regras Globais', room ? `Regras da sala ${room.title}` : ``],
+				spanish: ['Por favor sigue las reglas:', 'pages/rules-es', 'Reglas Globales', room ? `Reglas de la sala ${room.title}` : ``],
+				italian: ['Per favore, rispetta le seguenti regole:', 'pages/rules-it', 'Regole Globali', room ? `Regole della room ${room.title}` : ``],
+				french: ['Veuillez suivre ces règles:', 'pages/rules-fr', 'Règles Générales', room ? `Règles de la room ${room.title}` : ``],
+				simplifiedchinese: ['请遵守规则:', 'pages/rules-zh', '全站规则', room ? `${room.title}房间规则` : ``],
+				traditionalchinese: ['請遵守規則:', 'pages/rules-tw', '全站規則', room ? `${room.title}房間規則` : ``],
+				japanese: ['ルールを守ってください:', 'pages/rules-ja', '全部屋共通ルール', room ? `${room.title}部屋のルール` : ``],
+				hindi: ['कृपया इन नियमों का पालन करें:', 'pages/rules-hi', 'आप सभी के लिए नियम:', room ? `${room.title} इस Room के नियम:` : ``],
+				turkish: ['Lütfen kurallara uyun:', 'pages/rules-tr', 'Genel kurallar', room ? `${room.title} odası kuralları` : ``],
+				dutch: ['Volg de regels:', 'pages/rules-nl', 'Globale Regels ', room ? `Regels van de ${room.title} room` : ``],
+				german: ['Bitte befolgt die Regeln:', 'pages/rules-de', 'Globale Regeln', room ? `Regeln des ${room.title} Raumes` : ``],
+				english: ['Please follow the rules:', 'rules', 'Global Rules', room ? `${room.title} room rules` : ``],
 			};
 			if (!this.runBroadcast()) return;
+			const globalRulesLink = `https://pokemonshowdown.com/${languageTable[room && room.language ? room.language : 'english'][1]}`;
+			const globalRulesLinkText = languageTable[room && room.language ? room.language : 'english'][2];
 			this.sendReplyBox(
-				`${languageTable[room.language || 'english'][0]}<br />` +
+				`${room ? languageTable[room.language || 'english'][0] + '<br />' : ``}` +
 				(room && room.rulesLink ? Chat.html`- <a href="${room.rulesLink}">${languageTable[room.language || 'english'][3]}</a><br />` : ``) +
-				`- <a href="https://pokemonshowdown.com/${languageTable[room.language || 'english'][1]}">${languageTable[room.language || 'english'][2]}</a>`
+				`- <a href="${globalRulesLink}">${globalRulesLinkText}</a>`
 			);
 			return;
 		}