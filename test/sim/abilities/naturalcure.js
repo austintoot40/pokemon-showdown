austintoot40/pokemon-showdown

commit 2097b3c7118d530c25f049b3cd12b9f2fd2005e7

Refactor dragIn to delegate to switchIn
dragIn and switchIn being two separate functions is a weird historical
quirk that leads to inconsistencies in implementation.

The only reason they need to be separated is a Mold Breaker quirk:
1da65efb123ff69

(This is now done with an `if` statement.)

This should fix a lot of Roar/Whirlwind mechanics issues from `dragIn`
being on outdated mechanics compared to `switchIn`.

sim/battle.ts
@@ -282,8 +282,9 @@ export class Battle {
 		this.prng = new PRNG(this.prng.startingSeed);
 	}
 
-	suppressingAttackEvents() {
-		return this.activePokemon && this.activePokemon.isActive && this.activeMove && this.activeMove.ignoreAbility;
+	suppressingAttackEvents(target?: Pokemon) {
+		return this.activePokemon && this.activePokemon.isActive && this.activePokemon !== target &&
+			this.activeMove && this.activeMove.ignoreAbility;
 	}
 
 	setActiveMove(move?: ActiveMove | null, pokemon?: Pokemon | null, target?: Pokemon | null) {
@@ -668,7 +669,7 @@ export class Battle {
 				continue;
 			}
 			if (status.effectType === 'Ability' && !status.isUnbreakable &&
-					this.suppressingAttackEvents() && this.activePokemon !== thing) {
+					this.suppressingAttackEvents(thing as Pokemon)) {
 				// ignore attacking events
 				const AttackingEvents = {
 					BeforeMove: 1,
@@ -1227,7 +1228,7 @@ export class Battle {
 		return true;
 	}
 
-	switchIn(pokemon: Pokemon, pos: number, sourceEffect: Effect | null = null) {
+	switchIn(pokemon: Pokemon, pos: number, sourceEffect: Effect | null = null, isDrag?: boolean) {
 		if (!pokemon || pokemon.isActive) {
 			this.hint("A switch failed because the Pokémon trying to switch in is already in.");
 			return false;
@@ -1270,10 +1271,39 @@ export class Battle {
 		for (const moveSlot of pokemon.moveSlots) {
 			moveSlot.used = false;
 		}
-		this.add('switch', pokemon, pokemon.getDetails);
+		this.add(isDrag ? 'drag' : 'switch', pokemon, pokemon.getDetails);
 		if (sourceEffect) this.log[this.log.length - 1] += `|[from]${sourceEffect.fullname}`;
+
 		this.queue.insertChoice({choice: 'runUnnerve', pokemon});
-		this.queue.insertChoice({choice: 'runSwitch', pokemon});
+		if (isDrag && this.gen >= 5) {
+			// runSwitch happens immediately so that Mold Breaker can make hazards bypass Clear Body and Levitate
+			this.runSwitch(pokemon);
+		} else {
+			this.queue.insertChoice({choice: 'runSwitch', pokemon});
+		}
+
+		return true;
+	}
+	runSwitch(pokemon: Pokemon) {
+		this.runEvent('Swap', pokemon);
+		this.runEvent('SwitchIn', pokemon);
+		if (this.gen <= 2 && !pokemon.side.faintedThisTurn && pokemon.draggedIn !== this.turn) {
+			this.runEvent('AfterSwitchInSelf', pokemon);
+		}
+		if (!pokemon.hp) return false;
+		pokemon.isStarted = true;
+		if (!pokemon.fainted) {
+			this.singleEvent('Start', pokemon.getAbility(), pokemon.abilityData, pokemon);
+			pokemon.abilityOrder = this.abilityOrder++;
+			this.singleEvent('Start', pokemon.getItem(), pokemon.itemData, pokemon);
+		}
+		if (this.gen === 4) {
+			for (const foeActive of pokemon.side.foe.active) {
+				foeActive.removeVolatile('substitutebroken');
+			}
+		}
+		pokemon.draggedIn = null;
+		return true;
 	}
 
 	canSwitch(side: Side) {
@@ -1296,55 +1326,17 @@ export class Battle {
 		return canSwitchIn;
 	}
 
-	dragIn(side: Side, pos?: number) {
-		if (!pos) pos = 0;
-		if (pos >= side.active.length) return false;
+	dragIn(side: Side, pos: number) {
 		const pokemon = this.getRandomSwitchable(side);
 		if (!pokemon || pokemon.isActive) return false;
-		pokemon.isActive = true;
-		this.runEvent('BeforeSwitchIn', pokemon);
-		if (side.active[pos]) {
-			const oldActive = side.active[pos];
-			if (!oldActive.hp) {
-				return false;
-			}
-			if (!this.runEvent('DragOut', oldActive)) {
-				return false;
-			}
-			this.runEvent('SwitchOut', oldActive);
-			oldActive.illusion = null;
-			this.singleEvent('End', oldActive.getAbility(), oldActive.abilityData, oldActive);
-			oldActive.isActive = false;
-			oldActive.isStarted = false;
-			oldActive.usedItemThisTurn = false;
-			oldActive.position = pokemon.position;
-			pokemon.position = pos;
-			side.pokemon[pokemon.position] = pokemon;
-			side.pokemon[oldActive.position] = oldActive;
-			this.queue.cancelMove(oldActive);
-			oldActive.clearVolatile();
+		const oldActive = side.active[pos];
+		if (!oldActive) throw new Error(`nothing to drag out`);
+		if (!oldActive.hp) return false;
+
+		if (!this.runEvent('DragOut', oldActive)) {
+			return false;
 		}
-		side.active[pos] = pokemon;
-		pokemon.activeTurns = 0;
-		pokemon.activeMoveActions = 0;
-		if (this.gen === 2) pokemon.draggedIn = this.turn;
-		for (const moveSlot of pokemon.moveSlots) {
-			moveSlot.used = false;
-		}
-		this.add('drag', pokemon, pokemon.getDetails);
-		if (this.gen >= 5) {
-			this.singleEvent('PreStart', pokemon.getAbility(), pokemon.abilityData, pokemon);
-			this.runEvent('Swap', pokemon);
-			this.runEvent('SwitchIn', pokemon);
-			if (!pokemon.hp) return true;
-			pokemon.isStarted = true;
-			if (!pokemon.fainted) {
-				this.singleEvent('Start', pokemon.getAbility(), pokemon.abilityData, pokemon);
-				this.singleEvent('Start', pokemon.getItem(), pokemon.itemData, pokemon);
-			}
-		} else {
-			this.queue.insertChoice({choice: 'runSwitch', pokemon});
-		}
+		if (!this.switchIn(pokemon, pos, null, true)) return false;
 		return true;
 	}
 
@@ -2544,24 +2536,7 @@ export class Battle {
 			this.singleEvent('PreStart', action.pokemon.getAbility(), action.pokemon.abilityData, action.pokemon);
 			break;
 		case 'runSwitch':
-			this.runEvent('Swap', action.pokemon);
-			this.runEvent('SwitchIn', action.pokemon);
-			if (this.gen <= 2 && !action.pokemon.side.faintedThisTurn && action.pokemon.draggedIn !== this.turn) {
-				this.runEvent('AfterSwitchInSelf', action.pokemon);
-			}
-			if (!action.pokemon.hp) break;
-			action.pokemon.isStarted = true;
-			if (!action.pokemon.fainted) {
-				this.singleEvent('Start', action.pokemon.getAbility(), action.pokemon.abilityData, action.pokemon);
-				action.pokemon.abilityOrder = this.abilityOrder++;
-				this.singleEvent('Start', action.pokemon.getItem(), action.pokemon.itemData, action.pokemon);
-			}
-			if (this.gen === 4) {
-				for (const foeActive of action.pokemon.side.foe.active) {
-					foeActive.removeVolatile('substitutebroken');
-				}
-			}
-			action.pokemon.draggedIn = null;
+			this.runSwitch(action.pokemon);
 			break;
 		case 'runPrimal':
 			if (!action.pokemon.transformed) {

data/abilities.js
@@ -3918,7 +3918,7 @@ let BattleAbilities = {
 	"stickyhold": {
 		shortDesc: "This Pokemon cannot lose its held item due to another Pokemon's attack.",
 		onTakeItem(item, pokemon, source) {
-			if (this.suppressingAttackEvents() && pokemon !== this.activePokemon || !pokemon.hp || pokemon.item === 'stickybarb') return;
+			if (this.suppressingAttackEvents(pokemon) || !pokemon.hp || pokemon.item === 'stickybarb') return;
 			if (!this.activeMove) throw new Error("Battle.activeMove is null");
 			if ((source && source !== pokemon) || this.activeMove.id === 'knockoff') {
 				this.add('-activate', pokemon, 'ability: Sticky Hold');

data/mods/gen4/abilities.js
@@ -338,7 +338,7 @@ let BattleAbilities = {
 	"stickyhold": {
 		inherit: true,
 		onTakeItem(item, pokemon, source) {
-			if (this.suppressingAttackEvents() && pokemon !== this.activePokemon) return;
+			if (this.suppressingAttackEvents(pokemon)) return;
 			if ((source && source !== pokemon) || (this.activeMove && this.activeMove.id === 'knockoff')) {
 				this.add('-activate', pokemon, 'ability: Sticky Hold');
 				return false;

data/mods/gennext/statuses.js
@@ -132,7 +132,7 @@ let BattleStatuses = {
 	unown: {
 		// Unown: Shadow Tag
 		onImmunity(type, pokemon) {
-			if (type === 'Ground' && (!this.suppressingAttackEvents() || this.activePokemon === pokemon)) return false;
+			if (type === 'Ground' && !this.suppressingAttackEvents(pokemon)) return false;
 		},
 		onStart(pokemon) {
 			if (pokemon.ability === 'levitate') {
@@ -146,7 +146,7 @@ let BattleStatuses = {
 	bronzong: {
 		// Bronzong: Heatproof
 		onImmunity(type, pokemon) {
-			if (type === 'Ground' && (!this.suppressingAttackEvents() || this.activePokemon === pokemon)) return false;
+			if (type === 'Ground' && !this.suppressingAttackEvents(pokemon)) return false;
 		},
 		onStart(pokemon) {
 			if (pokemon.ability === 'levitate') {
@@ -158,7 +158,7 @@ let BattleStatuses = {
 	weezing: {
 		// Weezing: Aftermath
 		onImmunity(type, pokemon) {
-			if (type === 'Ground' && (!this.suppressingAttackEvents() || this.activePokemon === pokemon)) return false;
+			if (type === 'Ground' && !this.suppressingAttackEvents(pokemon)) return false;
 		},
 		onStart(pokemon) {
 			if (pokemon.ability === 'levitate') {
@@ -170,7 +170,7 @@ let BattleStatuses = {
 	flygon: {
 		// Flygon: Compoundeyes
 		onImmunity(type, pokemon) {
-			if (type === 'Ground' && (!this.suppressingAttackEvents() || this.activePokemon === pokemon)) return false;
+			if (type === 'Ground' && !this.suppressingAttackEvents(pokemon)) return false;
 		},
 		onStart(pokemon) {
 			if (pokemon.ability === 'levitate') {
@@ -182,7 +182,7 @@ let BattleStatuses = {
 	eelektross: {
 		// Eelektross: Poison Heal
 		onImmunity(type, pokemon) {
-			if (type === 'Ground' && (!this.suppressingAttackEvents() || this.activePokemon === pokemon)) return false;
+			if (type === 'Ground' && !this.suppressingAttackEvents(pokemon)) return false;
 		},
 		onStart(pokemon) {
 			if (pokemon.ability === 'levitate') {
@@ -194,7 +194,7 @@ let BattleStatuses = {
 	claydol: {
 		// Claydol: Filter
 		onImmunity(type, pokemon) {
-			if (type === 'Ground' && (!this.suppressingAttackEvents() || this.activePokemon === pokemon)) return false;
+			if (type === 'Ground' && !this.suppressingAttackEvents(pokemon)) return false;
 		},
 		onStart(pokemon) {
 			if (pokemon.ability === 'levitate') {
@@ -206,7 +206,7 @@ let BattleStatuses = {
 	gengar: {
 		// Gengar: Cursed Body
 		onImmunity(type, pokemon) {
-			if (pokemon.species.id !== 'gengarmega' && type === 'Ground' && (!this.suppressingAttackEvents() || this.activePokemon === pokemon)) return false;
+			if (pokemon.species.id !== 'gengarmega' && type === 'Ground' && !this.suppressingAttackEvents(pokemon)) return false;
 		},
 		onStart(pokemon) {
 			if (pokemon.ability === 'levitate') {
@@ -218,7 +218,7 @@ let BattleStatuses = {
 	mismagius: {
 		// Mismagius: Cursed Body
 		onImmunity(type, pokemon) {
-			if (type === 'Ground' && (!this.suppressingAttackEvents() || this.activePokemon === pokemon)) return false;
+			if (type === 'Ground' && !this.suppressingAttackEvents(pokemon)) return false;
 		},
 		onStart(pokemon) {
 			if (pokemon.ability === 'levitate') {
@@ -230,7 +230,7 @@ let BattleStatuses = {
 	mesprit: {
 		// Mesprit: Serene Grace
 		onImmunity(type, pokemon) {
-			if (type === 'Ground' && (!this.suppressingAttackEvents() || this.activePokemon === pokemon)) return false;
+			if (type === 'Ground' && !this.suppressingAttackEvents(pokemon)) return false;
 		},
 		onStart(pokemon) {
 			if (pokemon.ability === 'levitate') {
@@ -242,7 +242,7 @@ let BattleStatuses = {
 	uxie: {
 		// Uxie: Synchronize
 		onImmunity(type, pokemon) {
-			if (type === 'Ground' && (!this.suppressingAttackEvents() || this.activePokemon === pokemon)) return false;
+			if (type === 'Ground' && !this.suppressingAttackEvents(pokemon)) return false;
 		},
 		onStart(pokemon) {
 			if (pokemon.ability === 'levitate') {
@@ -254,7 +254,7 @@ let BattleStatuses = {
 	azelf: {
 		// Azelf: Steadfast
 		onImmunity(type, pokemon) {
-			if (type === 'Ground' && (!this.suppressingAttackEvents() || this.activePokemon === pokemon)) return false;
+			if (type === 'Ground' && !this.suppressingAttackEvents(pokemon)) return false;
 		},
 		onStart(pokemon) {
 			if (pokemon.ability === 'levitate') {
@@ -266,7 +266,7 @@ let BattleStatuses = {
 	hydreigon: {
 		// Hydreigon: Sheer Force
 		onImmunity(type, pokemon) {
-			if (type === 'Ground' && (!this.suppressingAttackEvents() || this.activePokemon === pokemon)) return false;
+			if (type === 'Ground' && !this.suppressingAttackEvents(pokemon)) return false;
 		},
 		onStart(pokemon) {
 			if (pokemon.ability === 'levitate') {
@@ -278,7 +278,7 @@ let BattleStatuses = {
 	rotom: {
 		// All Rotoms: Trace
 		onImmunity(type, pokemon) {
-			if (type === 'Ground' && (!this.suppressingAttackEvents() || this.activePokemon === pokemon)) return false;
+			if (type === 'Ground' && !this.suppressingAttackEvents(pokemon)) return false;
 		},
 		onStart(pokemon) {
 			if (pokemon.ability === 'levitate') {
@@ -290,7 +290,7 @@ let BattleStatuses = {
 	rotomheat: {
 		// All Rotoms: Trace
 		onImmunity(type, pokemon) {
-			if (type === 'Ground' && (!this.suppressingAttackEvents() || this.activePokemon === pokemon)) return false;
+			if (type === 'Ground' && !this.suppressingAttackEvents(pokemon)) return false;
 		},
 		onStart(pokemon) {
 			if (pokemon.ability === 'levitate') {
@@ -302,7 +302,7 @@ let BattleStatuses = {
 	rotomwash: {
 		// All Rotoms: Trace
 		onImmunity(type, pokemon) {
-			if (type === 'Ground' && (!this.suppressingAttackEvents() || this.activePokemon === pokemon)) return false;
+			if (type === 'Ground' && !this.suppressingAttackEvents(pokemon)) return false;
 		},
 		onStart(pokemon) {
 			if (pokemon.ability === 'levitate') {
@@ -314,7 +314,7 @@ let BattleStatuses = {
 	rotomfan: {
 		// All Rotoms: Trace
 		onImmunity(type, pokemon) {
-			if (type === 'Ground' && (!this.suppressingAttackEvents() || this.activePokemon === pokemon)) return false;
+			if (type === 'Ground' && !this.suppressingAttackEvents(pokemon)) return false;
 		},
 		onStart(pokemon) {
 			if (pokemon.ability === 'levitate') {
@@ -326,7 +326,7 @@ let BattleStatuses = {
 	rotomfrost: {
 		// All Rotoms: Trace
 		onImmunity(type, pokemon) {
-			if (type === 'Ground' && (!this.suppressingAttackEvents() || this.activePokemon === pokemon)) return false;
+			if (type === 'Ground' && !this.suppressingAttackEvents(pokemon)) return false;
 		},
 		onStart(pokemon) {
 			if (pokemon.ability === 'levitate') {
@@ -338,7 +338,7 @@ let BattleStatuses = {
 	rotommow: {
 		// All Rotoms: Trace
 		onImmunity(type, pokemon) {
-			if (type === 'Ground' && (!this.suppressingAttackEvents() || this.activePokemon === pokemon)) return false;
+			if (type === 'Ground' && !this.suppressingAttackEvents(pokemon)) return false;
 		},
 		onStart(pokemon) {
 			if (pokemon.ability === 'levitate') {
@@ -363,7 +363,7 @@ let BattleStatuses = {
 			}
 		},
 		onImmunity(type, pokemon) {
-			if (type === 'Ground' && (!this.suppressingAttackEvents() || this.activePokemon === pokemon)) return false;
+			if (type === 'Ground' && !this.suppressingAttackEvents(pokemon)) return false;
 		},
 		onStart(pokemon) {
 			if (pokemon.ability === 'levitate') {

sim/battle-queue.ts
@@ -99,6 +99,8 @@ export interface PokemonAction {
 	speed: number;
 	/** the pokemon doing action */
 	pokemon: Pokemon;
+	/** `runSwitch` only: the pokemon forcing this pokemon to switch in */
+	dragger?: Pokemon;
 }
 
 export type Action = MoveAction | SwitchAction | TeamAction | FieldAction | PokemonAction;

commit fd98c25bb5a42e5df38ae246b197314dd7b8fbda

Fix `Battle#switchIn` (#6521)
Regression caused by: https://github.com/smogon/pokemon-showdown/commit/2097b3c7118d530c25f049b3cd12b9f2fd2005e7

`Battle#dragIn` used to run the `SwitchOut` event among other functions
but were accidentally left off in the refactor. This commit adds them back.

The main bug caused by this is Natural Cure not curing status aliments when
phased out by Roar.

sim/battle.ts
@@ -1256,6 +1256,11 @@ export class Battle {
 		pokemon.isActive = true;
 		this.runEvent('BeforeSwitchIn', pokemon);
 		if (oldActive) {
+			if (isDrag) {
+				this.runEvent('SwitchOut', oldActive);
+				oldActive.illusion = null;
+				this.singleEvent('End', oldActive.getAbility(), oldActive.abilityData, oldActive);
+			}
 			oldActive.isActive = false;
 			oldActive.isStarted = false;
 			oldActive.usedItemThisTurn = false;
@@ -1272,6 +1277,7 @@ export class Battle {
 			moveSlot.used = false;
 		}
 		this.add(isDrag ? 'drag' : 'switch', pokemon, pokemon.getDetails);
+		if (isDrag && this.gen === 2) pokemon.draggedIn = this.turn;
 		if (sourceEffect) this.log[this.log.length - 1] += `|[from]${sourceEffect.fullname}`;
 		pokemon.previouslySwitchedIn++;
 

test/sim/abilities/naturalcure.js
@@ -1,0 +1,26 @@
+'use strict';
+
+const assert = require('./../../assert');
+const common = require('./../../common');
+
+let battle;
+
+describe('Natural Cure', function () {
+	it('should cure even when phased out by Roar', function () {
+		battle = common.createBattle();
+		battle.setPlayer('p1', {
+			team: [
+				{species: 'Celebi', ability: 'naturalcure', moves: ['leechseed']},
+				{species: 'Swampert', ability: 'torrents', moves: ['surf']},
+			],
+		});
+		battle.setPlayer('p2', {
+			team: [
+				{species: 'Zapdos', ability: 'pressure', moves: ['thunderwave', 'roar']},
+			],
+		});
+		battle.makeChoices('move leechseed', 'move thunderwave');
+		battle.makeChoices('move leechseed', 'move roar');
+		assert.false(battle.p1.pokemon[1].status === 'par');
+	});
+});