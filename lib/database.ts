austintoot40/pokemon-showdown

commit 7c492831e8af6ef60a56d3d18273997a90b6fb9e

Database: Remove LIMIT clauses from primary key mutating methods
PG doesn't support LIMIT in these, and they don't need it anyway since primary keys should be unique.

lib/database.ts
@@ -321,11 +321,11 @@ export class DatabaseTable<Row, DB extends Database> {
 	}
 	delete(primaryKey: BasicSQLValue) {
 		if (!this.primaryKeyName) throw new Error(`Cannot delete() without a single-column primary key`);
-		return this.deleteAll()`WHERE "${this.primaryKeyName}" = ${primaryKey} LIMIT 1`;
+		return this.deleteAll()`WHERE "${this.primaryKeyName}" = ${primaryKey}`;
 	}
 	update(primaryKey: BasicSQLValue, data: PartialOrSQL<Row>) {
 		if (!this.primaryKeyName) throw new Error(`Cannot update() without a single-column primary key`);
-		return this.updateAll(data)`WHERE "${this.primaryKeyName}" = ${primaryKey} LIMIT 1`;
+		return this.updateAll(data)`WHERE "${this.primaryKeyName}" = ${primaryKey}`;
 	}
 }
 
@@ -335,7 +335,6 @@ export class MySQLDatabase extends Database<mysql.Pool, mysql.OkPacket> {
 		const prefix = config.prefix || "";
 		if (config.prefix) {
 			config = { ...config };
-			delete config.prefix;
 		}
 		super(mysql.createPool(config), prefix);
 	}

commit e1560f9867c8813d8245ad5701884ebde20b8845

Fix MySQL support in db library
(Broken in 7c492831e8af6)

lib/database.ts
@@ -336,6 +336,7 @@ export class MySQLDatabase extends Database<mysql.Pool, mysql.OkPacket> {
 		const prefix = config.prefix || "";
 		if (config.prefix) {
 			config = { ...config };
+			delete config.prefix;
 		}
 		super(mysql.createPool(config), prefix);
 	}